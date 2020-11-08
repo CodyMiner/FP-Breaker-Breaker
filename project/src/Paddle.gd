9c35786033282c10202ef6038a43099d6524b838
cody <cminer145@gmail.com>
2020-11-08 06:16:59 -0500

    Bind paddle to screen's edge

--- project/src/Paddle.gd
+++ project/src/Paddle.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 const SPEED := 5.0
 
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("go_left"):
-		self.move_and_collide(Vector2(-SPEED, 0))
+		move_and_collide(Vector2(-SPEED, 0))
 	elif Input.is_action_pressed("go_right"):
-		self.move_and_collide(Vector2(SPEED, 0))
+		move_and_collide(Vector2(SPEED, 0))
