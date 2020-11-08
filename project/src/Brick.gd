bfbb7be717eb215da493d1ebd94ef290b81f8974
cody <cminer145@gmail.com>
2020-11-08 09:22:49 -0500

    Allow bricks to be deleted when touched

--- project/src/Brick.gd
+++ project/src/Brick.gd
@@ -1,1 +1,4 @@
-extends StaticBody2D
+extends Area2D
+
+func _on_Bricks_body_entered(body):
+	self.queue_free()
--- project/src/Paddle.gd
+++ project/src/Paddle.gd
@@ -2,8 +2,13 @@ extends KinematicBody2D
 
 const SPEED := 5.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	var collision_info : KinematicCollision2D
+	
 	if Input.is_action_pressed("go_left"):
-		move_and_collide(Vector2(-SPEED, 0))
+		collision_info = move_and_collide(Vector2(-SPEED, 0))
 	elif Input.is_action_pressed("go_right"):
-		move_and_collide(Vector2(SPEED, 0))
+		collision_info =move_and_collide(Vector2(SPEED, 0))
+	
+	if collision_info:
+		pass
