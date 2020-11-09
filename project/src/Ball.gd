20c666e5458b5cb857bd362351f751fc2a8570a3
cody <cminer145@gmail.com>
2020-11-09 03:38:07 -0500

    Implement win screen

--- project/src/Ball.gd
+++ project/src/Ball.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
+var bricks_broken := 0
+
 func _on_Ball_body_entered(body):
 	if body.has_method("hit"):
 		body.hit()
+		bricks_broken += 1
+		if bricks_broken > 35:
+			get_tree().change_scene("res://src/win.tscn")
 	elif body.has_method("gameover"):
 		body.gameover()
 
