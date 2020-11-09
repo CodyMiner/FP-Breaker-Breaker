7f473d90f3892618303dff754fb4daa6489c1d45
cody <cminer145@gmail.com>
2020-11-09 03:45:49 -0500

    Correct minor style guide violations

--- project/src/pause_dialog.gd
+++ project/src/pause_dialog.gd
@@ -1,25 +1,22 @@
 extends Popup
 
-onready var delegate  = self 
+onready var delegate := self 
 
 func _draw():
 	var r = Rect2($CenterContainer.rect_position,$CenterContainer.rect_size)
 	draw_rect(r,Color(1,1,1,.5))
 
-			
+
 func _input(event):
 	if event.is_action("ui_cancel")  : 
 		get_tree().set_input_as_handled()
 		_on_btn_continue_pressed()
-		
-		
+
+
 func _on_btn_quit_pressed():
 	get_tree().change_scene("res://src/TitleScreen.tscn")
-
-
 	
 func _on_btn_continue_pressed():
 	var response = {"message":"Continue"}
 	if delegate.has_method("close_dialog"):
 		delegate.close_dialog(self,response)
-
