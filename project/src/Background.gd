2b85c12529d4fba14675eab0996c15dbc8ab7bd2
Janardhen Jayavel <52244557+Janardhen@users.noreply.github.com>
2020-11-09 02:00:07 -0500

    test

--- project/src/Background.gd
+++ project/src/Background.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+onready var pause_dialog_desource = preload("res://src/pause_dialog.tscn")
+
+
+
 func _setup_bricks():
 	randomize()
 	for row in range(6):
@@ -12,3 +16,27 @@ func _setup_bricks():
 
 func _ready():
 	_setup_bricks()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action("ui_cancel") and not event.is_pressed():
+		get_tree().set_input_as_handled()
+		set_process_input(false)
+		show_pause_dialog()
+
+func show_pause_dialog():
+	var pause_dialog_node = pause_dialog_desource.instance()
+	self.add_child(pause_dialog_node)
+	pause_dialog_node.delegate = self 
+	pause_dialog_node.popup()
+	get_tree().set_pause(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func close_dialog(dialog,response):
+	dialog.queue_free()
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	get_tree().set_pause(false)
+	set_process_input(true) 
+	if(response.message=="Continue"):
+		pass 
--- project/src/Brick.gd
+++ project/src/Brick.gd
@@ -1,10 +1,12 @@
-extends Area2D
+extends StaticBody2D
 
-func _on_Bricks_body_entered(body):
-	self.queue_free()
+#func _on_Bricks_body_entered(body):
+#	self.queue_free()
 
 func _ready():
-	#self.Timer.set_wait_time(rand_range(0.0, 2.0))
 	var time := rand_range(0.0, 1.0)
 	yield(get_tree().create_timer(time), "timeout")
 	self.visible = true
+
+func hit():
+	queue_free()
