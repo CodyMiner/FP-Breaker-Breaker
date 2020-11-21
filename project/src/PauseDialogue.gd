b32c7c030f5095134d19c0fa9163e765ca9e55d2
cody <cminer145@gmail.com>
2020-11-21 17:23:00 -0500

    Correct style guide error

--- project/src/PauseDialogue.gd
+++ project/src/PauseDialogue.gd
@@ -3,7 +3,6 @@ extends Popup
 var my_scene = load("res://src/Background.tscn").instance()
 
 
-
 func _draw():
 	var r = Rect2($CenterContainer.rect_position,$CenterContainer.rect_size)
 	draw_rect(r,Color(1,1,1,.5))
@@ -19,7 +18,8 @@ func _input(event):
 func _on_btn_quit_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to(my_scene)
-	
+
+
 func _on_btn_continue_pressed():
 	var response = {"message":"Continue"}
 	if my_scene.has_method("close_dialog"):
